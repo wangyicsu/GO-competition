2243acf38e8c28e0e67303b89c40cac938112994
wangyicsu <wangyiccnu@gmail.com>
2017-12-03 14:11:48 -0700

    Delete solveopf.m

--- solveopf.m
+++
@@ -1,8 +0,0 @@
-function result = solveopf
-    mat = csvread('generator.csv',1,2);
-    MPC = psse2mpc('powersystem.raw');
-    MPC_GO = changemate(mat,MPC);
-    result = runopf(MPC_GO);
-    writefile(result);
-end
-  
